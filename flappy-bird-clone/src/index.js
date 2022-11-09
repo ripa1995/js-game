708a39805b5cc3872afff3cd1d8aaf555f28b58b
ripa1995 <roberto.ripamonti@ymail.com>
2022-11-09 19:28:40 +0100

    refactor: split play scene code from game init

--- flappy-bird-clone/src/index.js
+++ flappy-bird-clone/src/index.js
@@ -1,11 +1,20 @@
-import Phaser, { UP } from 'phaser';
+import Phaser from 'phaser';
+import PlayScene from './scenes/playScene';
+
+const WIDTH = 800;
+const HEIGHT = 600;
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT
+}
 
 //https://photonstorm.github.io/phaser3-docs/Phaser.Types.Core.html#.GameConfig
 const CONFIG = {
   //WebGL (Web graphics library) JS Api for rendering 2D/3D graphics
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: WIDTH,
+  height: HEIGHT,
   physics: {
     //Arcade physics plugin, manages physics simulation
     default: 'arcade',
@@ -17,131 +26,7 @@ const CONFIG = {
     }
   },
   //https://photonstorm.github.io/phaser3-docs/Phaser.Scene.html
-  scene: {
-    preload, //https://photonstorm.github.io/phaser3-docs/Phaser.Types.Scenes.html#.ScenePreloadCallback
-    create, //https://photonstorm.github.io/phaser3-docs/Phaser.Types.Scenes.html#.SceneCreateCallback
-    update //https://photonstorm.github.io/phaser3-docs/Phaser.Scene.html#update
-  }
+  scene: [new PlayScene(SHARED_CONFIG)]
 }
-
-const Y_GRAVITY = 400;
-const DEFAULT_Y_VELOCITY = 0;
-const DEFAULT_X_POS = CONFIG.width / 10;
-const DEFAULT_Y_POS = CONFIG.height / 2;
-const PIPE_Y_DISTANCE_RANGE = [150, 250];
-const PIPE_TO_RENDER = 4;
-const PIPE_X_DISTANCE_RANGE = [400, 600];
-
-let bird = null;
-let pipeHorizontalDistance = 0;
-let flapVelocity = 250;
-let pipes = null;
-
-//Loading assets, such as images, music, animations, ...
-function preload() {
-  //this context -- scene
-  //contains functions and properties we can use
-  this.load.image('sky', './assets/sky.png')
-  this.load.image('bird', './assets/bird.png')
-  this.load.image('pipe', './assets/pipe.png')
-}
-
-//Initialising the app
-function create() {
-  //x,y,key of the image
-  //x,y will be canvas coordinates, hence giving 0,0 will print only right bottom quarter of the image, since origin coordinates of the image default to its center.
-  //with .setOrigin we change the origin of the image. where 0,0 is top left corner of the image, 1,1 is bottom right
-  //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObjectFactory.html#image__anchor
-  //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Image.html#setOrigin__anchor
-  this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObjectFactory.html#sprite__anchor
-  //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Sprite.html
-  bird = this.physics.add.sprite(DEFAULT_X_POS, DEFAULT_Y_POS, 'bird').setOrigin(0); //if y is missing defaults to X
-  //add gravity, it increases body velocity
-  //Acceleration due to gravity (specific to this Body), in pixels per second squared. Total gravity is the sum of this vector and the simulation's gravity.
-  //https://photonstorm.github.io/phaser3-docs/Phaser.Physics.Arcade.Body.html#gravity
-  bird.body.gravity.y = Y_GRAVITY; //px per seconds of accelleration. Sums up to scene gravity, does not replace it.
-
-  //add velocity -- affected by gravity as well
-  //The Body's velocity, in pixels per second
-  //https://photonstorm.github.io/phaser3-docs/Phaser.Physics.Arcade.Body.html#velocity
-  //bird.body.velocity.x = 100;
-
-  //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObjectFactory.html#group__anchor
-  pipes = this.physics.add.group();
-
-  for (let i = 0; i < PIPE_TO_RENDER; i++) {
-    //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Group.html#create__anchor
-    const upperPipe = pipes.create(0,0, 'pipe').setOrigin(0, 1); //draw the image from bottom left corner
-    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0); //draw the image from top left corner  
-  
-    placePipe(upperPipe, lowerPipe)
-  }
-
-  pipes.setVelocityX(-200);
-
-  //add listener on mouse and space click
-  //https://photonstorm.github.io/phaser3-docs/Phaser.Scene.html#input__anchor
-  this.input.on('pointerdown', flap)
-  this.input.keyboard.on('keydown_SPACE', flap)
-}
-
-//default: 60fps i.e. update executed 60 times per second
-function update(time /* ms time since first update */, delta /* ms delta time since last update*/) {
-  if (bird.y < 0 || bird.y > CONFIG.height) {
-    resetBird();
-  }
-  recyclePipes()
-}
-
-function placePipe(uPipe, lPipe) {
-  const rightMostPipeX = getRightMostPipe();
-  let pipeVerticalDistance = Phaser.Math.Between(...PIPE_Y_DISTANCE_RANGE);
-  let pipeVerticalPosition = Phaser.Math.Between(20, CONFIG.height - 20 - pipeVerticalDistance)
-  let pipeHorizontalDistance = Phaser.Math.Between(...PIPE_X_DISTANCE_RANGE);
-
-  uPipe.x = rightMostPipeX + pipeHorizontalDistance;
-  uPipe.y = pipeVerticalPosition;
-
-  lPipe.x = uPipe.x;
-  lPipe.y = uPipe.y + pipeVerticalDistance;
-}
-
-function recyclePipes() {
-  let tempPipes = [];
-  pipes.getChildren().forEach(pipe => {
-    if (pipe.getBounds().right <= 0) {
-      //recycle pipe
-      //get upper and lower pipe that are out of the bounds
-      tempPipes.push(pipe);
-      if (tempPipes.length == 2) {
-        placePipe(...tempPipes)
-        tempPipes = [];
-      }
-    }
-  })
-}
-
-function getRightMostPipe() {
-  let rightMostPipeX = 0;
-  
-
-  pipes.getChildren().forEach(pipe => {
-    rightMostPipeX = Math.max(pipe.x, rightMostPipeX);
-  });
-
-  return rightMostPipeX;
-}
-
-function resetBird() {
-  bird.y = DEFAULT_Y_POS;
-  bird.x = DEFAULT_X_POS;
-  bird.body.velocity.y = DEFAULT_Y_VELOCITY;
-}
-
-function flap() {
-  bird.body.velocity.y -= flapVelocity;
-}
-
 
 new Phaser.Game(CONFIG);
---
+++ flappy-bird-clone/src/scenes/playScene.js
@@ -0,0 +1,147 @@
+import Phaser from 'phaser';
+
+
+const Y_GRAVITY = 400;
+const FLAP_VELOCITY = 200;
+const PIPE_Y_DISTANCE_RANGE = [150, 250];
+const PIPE_TO_RENDER = 4;
+const PIPE_X_DISTANCE_RANGE = [400, 600];
+
+class PlayScene extends Phaser.Scene {
+
+    constructor(config) {
+        super("play-scene")
+        this.config = config;
+        this.bird = null;
+        this.pipes = null;
+        this.birdPosition = {x: this.config.width/10, y: this.config.height/2};
+    }
+
+    //https://photonstorm.github.io/phaser3-docs/Phaser.Types.Scenes.html#.ScenePreloadCallback
+    //Loading assets, such as images, music, animations, ...
+    preload() {
+        //this context -- scene
+        //contains functions and properties we can use
+        this.load.image('sky', './assets/sky.png')
+        this.load.image('bird', './assets/bird.png')
+        this.load.image('pipe', './assets/pipe.png')
+    }
+
+    //https://photonstorm.github.io/phaser3-docs/Phaser.Types.Scenes.html#.SceneCreateCallback
+    create() {
+        this.createBackground();
+        this.createBird();
+        this.createPipes();
+        this.handleInputs();
+    }
+
+    //https://photonstorm.github.io/phaser3-docs/Phaser.Scene.html#update
+    //default: 60fps i.e. update executed 60 times per second
+    update(time /* ms time since first update */, delta /* ms delta time since last update*/) {
+        this.checkGameStatus();
+        this.recyclePipes();
+    }
+
+    createBackground() {
+        //x,y,key of the image
+        //x,y will be canvas coordinates, hence giving 0,0 will print only right bottom quarter of the image, since origin coordinates of the image default to its center.
+        //with .setOrigin we change the origin of the image. where 0,0 is top left corner of the image, 1,1 is bottom right
+        //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObjectFactory.html#image__anchor
+        //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Image.html#setOrigin__anchor
+        this.add.image(0, 0, 'sky').setOrigin(0, 0);
+    }
+
+    createBird() {
+        //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObjectFactory.html#sprite__anchor
+        //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Sprite.html
+        this.bird = this.physics.add.sprite(this.birdPosition.x, this.birdPosition.y, 'bird').setOrigin(0); //if y is missing defaults to X
+        //add gravity, it increases body velocity
+        //Acceleration due to gravity (specific to this Body), in pixels per second squared. Total gravity is the sum of this vector and the simulation's gravity.
+        //https://photonstorm.github.io/phaser3-docs/Phaser.Physics.Arcade.Body.html#gravity
+        this.bird.body.gravity.y = Y_GRAVITY; //px per seconds of accelleration. Sums up to scene gravity, does not replace it.
+
+        //add velocity -- affected by gravity as well
+        //The Body's velocity, in pixels per second
+        //https://photonstorm.github.io/phaser3-docs/Phaser.Physics.Arcade.Body.html#velocity
+        //bird.body.velocity.x = 100;
+    }
+
+    createPipes() {
+        //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.GameObjectFactory.html#group__anchor
+        this.pipes = this.physics.add.group();
+
+        for (let i = 0; i < PIPE_TO_RENDER; i++) {
+            //https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Group.html#create__anchor
+            const upperPipe = this.pipes.create(0,0, 'pipe').setOrigin(0, 1); //draw the image from bottom left corner
+            const lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 0); //draw the image from top left corner  
+        
+            this.placePipe(upperPipe, lowerPipe)
+        }
+
+        this.pipes.setVelocityX(-200);
+    }
+
+    handleInputs() {
+        //add listener on mouse and space click
+        //https://photonstorm.github.io/phaser3-docs/Phaser.Scene.html#input__anchor
+        this.input.on('pointerdown', this.flap, this)
+        this.input.keyboard.on('keydown_SPACE', this.flap, this)
+    }
+
+    checkGameStatus() {
+        if (this.bird.y < 0 || this.bird.y > this.config.height) {
+            this.resetBird();
+        }
+    }
+  
+    placePipe(uPipe, lPipe) {
+        const rightMostPipeX = this.getRightMostPipe();
+        let pipeVerticalDistance = Phaser.Math.Between(...PIPE_Y_DISTANCE_RANGE);
+        let pipeVerticalPosition = Phaser.Math.Between(20, this.config.height - 20 - pipeVerticalDistance)
+        let pipeHorizontalDistance = Phaser.Math.Between(...PIPE_X_DISTANCE_RANGE);
+    
+        uPipe.x = rightMostPipeX + pipeHorizontalDistance;
+        uPipe.y = pipeVerticalPosition;
+    
+        lPipe.x = uPipe.x;
+        lPipe.y = uPipe.y + pipeVerticalDistance;
+    }
+  
+    recyclePipes() {
+        let tempPipes = [];
+        this.pipes.getChildren().forEach(pipe => {
+            if (pipe.getBounds().right <= 0) {
+                //recycle pipe
+                //get upper and lower pipe that are out of the bounds
+                tempPipes.push(pipe);
+                if (tempPipes.length == 2) {
+                this.placePipe(...tempPipes)
+                tempPipes = [];
+                }
+            }
+        })
+    }
+  
+    getRightMostPipe() {
+        let rightMostPipeX = 0;
+
+        this.pipes.getChildren().forEach(pipe => {
+            rightMostPipeX = Math.max(pipe.x, rightMostPipeX);
+        });
+    
+        return rightMostPipeX;
+    }
+  
+    resetBird() {
+        this.bird.y = this.birdPosition.y;
+        this.bird.x = this.birdPosition.x;
+        this.bird.body.velocity.y = 0;
+    }
+  
+    flap() {
+        this.bird.body.velocity.y -= FLAP_VELOCITY;
+    }
+  
+}
+
+export default PlayScene;
