1f8614b07c5254464c4a15a6f4491b4302ed052f
ripa1995 <roberto.ripamonti@ymail.com>
2022-11-14 19:32:34 +0100

    feat: add raycast to enemies

--- platform-game/src/entities/enemy.js
+++ platform-game/src/entities/enemy.js
@@ -18,6 +18,7 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
     init() {
         this.gravity = 500;
         this.speed = 150;
+        this.rayGraphics = this.scene.add.graphics({lineStyle: {width: 2, color: 'purple'}});
 
         this.body.setGravityY(this.gravity);
         this.setSize(20,45);
@@ -33,6 +34,20 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
 
     update(time, delta) {
         this.setVelocityX(20);
+        const ray = this.raycast(this.body);
+
+        this.rayGraphics.clear();
+        this.rayGraphics.strokeLineShape(ray);
+    }
+
+    raycast(body, raylength = 30) {
+        const { x, y, width, halfHeight} = body;
+        const line = new Phaser.Geom.Line();
+        line.x1 = x + width;
+        line.y1 = y + halfHeight;
+        line.x2 = line.x1 + width / 2;
+        line.y2 = line.y1 + halfHeight;
+        return line;
     }
 }
 
